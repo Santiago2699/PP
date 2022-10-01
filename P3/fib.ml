a460ee4dc16b1787cac3c82650a55996bf843e89
Santiago Castro <s.a.castro.rampersad@udc.es>
2022-10-02 00:57:04 +0200

    fib casi acabada

--- P3/fib.ml
+++ P3/fib.ml
@@ -1,3 +1,10 @@
-let rec fib n =
-  if n <= 1 then n
-  else fib (n-1) + fib (n-2);;
+
+ let rec fib n = 
+    if n <= 1 then n
+    else fib (n-1) + fib (n-2) 
+
+   let rec printTo n = 
+    if n <= 1 then ()
+    else printTo (n-1);
+     print_endline (string_of_int (fib n))
+    
